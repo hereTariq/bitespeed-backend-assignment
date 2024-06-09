2bf7285fc42136fca499c760933e74f5ef4d4adc
Mohammad Tariq <fk68790@gmail.com>
2024-06-09 22:15:19 +0530

    db connected

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,8 @@
-export const PORT = process.env.PORT || 3000;
+import dotenv from 'dotenv';
+dotenv.config();
+export const PORT = process.env.PORT || 4000;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASS = process.env.DB_PASS || 'password';
 export const DB_NAME = process.env.DB_NAME || 'fluxkart';
+export const DB_PORT = process.env.DB_PORT || 3600;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,15 @@
 import mysql from 'mysql2/promise';
-import { DB_HOST, DB_NAME, DB_PASS, DB_USER } from './constants';
-
+import { DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER } from './constants';
+console.log(DB_USER);
 const pool = mysql.createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASS,
     database: DB_NAME,
+    port: Number(DB_PORT),
+    // ssl: {
+
+    // }
 });
 
 (async () => {
